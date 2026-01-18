d9fc333ff8eb1a68c45ebee056f1692a555fcf0f
kshitizz-08 <kshitijkalhapure0806@gmail.com>
2026-01-18 15:25:19 +0530

    Fix: Express 5 compatibility - update wildcard route syntax

--- Desktop/kumbh-medical-system/project/server/index.js
+++ Desktop/kumbh-medical-system/project/server/index.js
@@ -41,7 +41,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // Handle client-side routing - serve index.html for all non-API routes
-app.get('*', (req, res) => {
+app.get(/^(?!\/api).+/, (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
